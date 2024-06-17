365c83263a74348818de4d77a3b5d4a980a3282c
acidlord <acidlord@yandex.ru>
2024-06-17 17:05:09 -0400

    Added summit to orders trigger

--- mswh/whChangeOrder.php
+++ mswh/whChangeOrder.php
@@ -67,6 +67,23 @@
 			        $log->write(__LINE__ . ' return - ' . json_encode ($return, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 			    }
 			}
+			if (isset ($orderData['project']['meta']['href']) ? APIMS::getIdFromHref($orderData['project']['meta']['href']) == MS_PROJECT_YANDEX_VYSOTA_ID : false)
+			{
+			    $campaign = BERU_API_VYSOTA_CAMPAIGN;
+			    
+			    $log->write(__LINE__ . ' campaign - ' . $campaign);
+			    
+			    $ordersYandexClass = new OrdersYandex($campaign);
+			    $orderDataYandex = $ordersYandexClass->getOrder ($orderData['name']);
+			    $log->write(__LINE__ . ' orderDataYandex - ' . json_encode ($orderDataYandex, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+			    
+			    if ($orderDataYandex['order']['status'] != 'CANCELLED')
+			    {
+			        //$return = $ordersYandexClass->updateStatus ($orderData['name'], 'CANCELLED', 'USER_CHANGED_MIND');
+			        $return = $ordersYandexClass->updateStatus ($campaign, $orderData['name'], 'CANCELLED', 'SHOP_FAILED');
+			        $log->write(__LINE__ . ' return - ' . json_encode ($return, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+			    }
+			}
 			if (isset ($orderData['project']['meta']['href']) ? APIMS::getIdFromHref($orderData['project']['meta']['href']) == MS_PROJECT_14DAYS_ALIANS : false)
 			{
 			    $campaign = BERU_API_ALIANS_CAMPAIGN;
@@ -377,6 +394,51 @@
 			{
 				//$organizationId = APIMS::getIdFromHref($orderData['organization']['meta']['href']);
 				$campaign = BERU_API_SUMMIT_CAMPAIGN;
+				$log->write(__LINE__ . ' campaign - ' . $campaign);
+
+				$ordersYandexClass = new OrdersYandex($campaign);
+				$orderInfo = $ordersYandexClass->getOrder($orderData['name']);
+				$log->write(__LINE__ . ' orderInfo - ' . json_encode ($orderInfo, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+				
+				// pack order
+				$packData = $ordersYandexClass->packOrder ($orderData['name'], $orderInfo['order']['delivery']);
+				$log->write(__LINE__ . ' packData - ' . json_encode ($packData, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+				
+				//get label data
+				$labelData = $ordersYandexClass->getOrderLabelData($orderData['name']);
+				
+				//update ms order data
+				$ordersMSClass = new OrdersMS();
+				$data = array(
+				    'attributes' => array(
+				        // адрес
+				        array(
+				            'meta' => APIMS::createMeta(MS_API_BASE_URL . MS_API_VERSION_1_2 . MS_API_CUSTOMERORDER . MS_API_ATTRIBUTES . '/' . MS_ADDRESS_ATTR, 'attributemetadata'),
+				            'value' => (string)$labelData['result']['parcelBoxLabels'][0]['deliveryAddress']
+				        ),
+				        // ФИО
+				        array(
+				            'meta' => APIMS::createMeta(MS_API_BASE_URL . MS_API_VERSION_1_2 . MS_API_CUSTOMERORDER . MS_API_ATTRIBUTES . '/' . MS_FIO_ATTR, 'attributemetadata'),
+				            'value' => (string)$labelData['result']['parcelBoxLabels'][0]['recipientName']
+				        ),
+				        // delivery number
+				        array(
+				            'meta' => APIMS::createMeta(MS_API_BASE_URL . MS_API_VERSION_1_2 . MS_API_CUSTOMERORDER . MS_API_ATTRIBUTES . '/' . MS_DELIVERYNUMBER_ATTR, 'attributemetadata'),
+				            'value' => (string)$labelData['result']['parcelBoxLabels'][0]['fulfilmentId']
+				        )
+				    )
+				);
+				$updateOrderResult = $ordersMSClass->updateCustomerorder($orderData['id'], $data);
+				$log->write(__LINE__ . ' updateOrderResult - ' . json_encode ($updateOrderResult, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+				
+				//update status
+				$return = $ordersYandexClass->updateStatus ($orderData['name'], 'PROCESSING', 'READY_TO_SHIP');
+				$log->write(__LINE__ . ' return - ' . json_encode ($return, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+			}
+			if (isset ($orderData['project']['meta']['href']) ? APIMS::getIdFromHref($orderData['project']['meta']['href']) == MS_PROJECT_YANDEX_VYSOTA_ID : false)
+			{
+				//$organizationId = APIMS::getIdFromHref($orderData['organization']['meta']['href']);
+				$campaign = BERU_API_VYSOTA_CAMPAIGN;
 				$log->write(__LINE__ . ' campaign - ' . $campaign);
 
 				$ordersYandexClass = new OrdersYandex($campaign);
