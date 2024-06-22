40ada867b86ab4a974d4519541494dc8a0991493
acidlord <acidlord@yandex.ru>
2024-06-22 19:02:47 -0400

    Updated offerMapping method on the new one fix7

--- beru-vysota/updatePrices.php
+++ beru-vysota/updatePrices.php
@@ -21,13 +21,12 @@ foreach ($skus as $key => $sku)
         foreach ($assortments as $assortment)
         {
             $price = $productsClass->getPrice($assortment, 'Цена СММ Альянс');
-            $vat = $assortment['effectiveVat'] == 20 ? 7 : ($assortment['effectiveVat'] == 10 ? 2 : ($assortment['effectiveVat'] == 0 ? 5 : 6));
+            //$vat = $assortment['effectiveVat'] == 20 ? 7 : ($assortment['effectiveVat'] == 10 ? 2 : ($assortment['effectiveVat'] == 0 ? 5 : 6));
             $data['offers'][] = array(
                 'offerId' => $assortment['code'],
                 'price' => array(
                     'value' => $price,
-                    'currencyId' => 'RUR',
-                    'vat' => $vat,
+                    'currencyId' => 'RUR'
                 )
             );
         }
