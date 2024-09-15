da53ad835ddac6a672c21e99de50798fafa2ea3c
acidlord <acidlord@yandex.ru>
2024-09-15 17:38:42 -0400

    ws supply creation fix 11

--- wildberriesKosmos/testSupply.php
+++ wildberriesKosmos/testSupply.php
@@ -27,7 +27,7 @@ foreach ($supplies as $supply)
 		break;
 	}
 
-echo 'supplies - ' . json_encode ($supplies, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+echo 'supplyOpen - ' . json_encode ($supplyOpen, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 return;
 
 if ($supplyOpen === null)
