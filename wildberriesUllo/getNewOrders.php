8de31e0cb724a83fe754fc4d404ffd22243b80a1
Georgy Polyan <georgy.polyan@gmail.com>
2025-01-19 14:25:24 -0500

    Updated wb ullo

--- wildberriesUllo/getNewOrders.php
+++ wildberriesUllo/getNewOrders.php
@@ -2,20 +2,21 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/MS/ordersMS.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/MS/productsMS.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Wildberries/Orders.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Wildberries/Products.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Wildberries/Supplies.php');
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Common/Log.php');
 
-$logger = new \Classes\Common\Log('wildberriesKaori - getNewOrders.log');
+$logger = new \Classes\Common\Log('wildberriesUllo - getNewOrders.log');
 
 $startDate = date('Y-m-d', strtotime('-2 days')) . 'T00:00:00.000+03:00';
 $endDate = NULL;
 $status = 0;
 
-$ordersWBClass = new \Classes\Wildberries\v1\Orders('Kaori');
+$ordersWBClass = new \Classes\Wildberries\v1\Orders('Ullo');
+$suppliesWBClass = new \Classes\Wildberries\v1\Supplies('U;;p');
 
-$newOrders = $ordersWBClass->orderList($startDate, $endDate, $status);
+$newOrders = $ordersWBClass->getNewOrders();
 
 if (!count($newOrders))
 {
@@ -23,7 +24,7 @@ if (!count($newOrders))
 	return;
 }
 
-$ordersIDs = array_column ($newOrders, 'orderId');
+$ordersIDs = array_column ($newOrders, 'id');
 $filter = '';
 foreach ($ordersIDs as $ordersID){
 	$filter .= 'name=WB' . $ordersID . ';';
@@ -33,72 +34,113 @@ $ordersMSClass = new OrdersMS();
 $ordersMS = $ordersMSClass->findOrders($filter);
 $ordersMSIDs = array_column ($ordersMS, 'name');
 
-$productsWBClass = new \Classes\Wildberries\v1\Products('Kaori');
-$productsWB = $productsWBClass->cardList();
-$productCodes = array();
-foreach ($productsWB as $product)
-{
-    if (isset($product['nomenclatures'][0]['variations'][0]['chrtId'])){
-        $productCodes[] = $product['nomenclatures'][0]['variations'][0]['chrtId'];
-    }
-    else{
-        $productCodes[] = null;
-    }
-}
-
 $productMSClass = new ProductsMS();
 $productMS0 = $productMSClass->findProductsByCode('000-0000');
 
 $newOrdersMS = array();
 $changeStatus = array();
-	
-foreach ($newOrders as $order)
-{
-	if (array_search('WB' . $order['orderId'], $ordersMSIDs) !== false)
+
+// check if supply exists
+$supplyOpen = null;
+$supplies = $suppliesWBClass->getSupplies();
+foreach ($supplies as $supply)
+	if ($supply['closedAt'] == null)
 	{
-		$logger->write ('Already loaded - ' . $order['orderId']);
+		$supplyOpen = $supply;
+		break;
+	}
+
+if ($supplyOpen === null)
+	$supplyOpen = $suppliesWBClass->createSupply('WB' . date('Y-m-d H:i:s'));
+
+foreach ($newOrders as $newOrder)
+{
+	if (array_search('WB' . $newOrder['id'], $ordersMSIDs) !== false)
+	{
+		$logger->write ('Already loaded - ' . $newOrder['id']);
 		continue;
 	}
-	
+		
 	$positions = array();
-	$prKey = array_search($order['chrtId'], $productCodes);
-	if ($prKey !== false)
-	{
-	    
-	    $productMS = $productMSClass->findProductsByCode($productsWB[$prKey]['supplierVendorCode']);
-		$productMS = !isset($productMS[0]) ? $productMS0 : $productMS;
-	}
-	else{
-		$productMS = $productMS0;
-	}
+
+	$productMS = $productMSClass->findProductsByCode($newOrder['article']);
+	$productMS = isset($productMS[0]) ? $productMS : $productMS0;
+
 	$position = array();
 	$position ['quantity'] = 1;
 	$position ['reserve'] = 1;
-	$position ['price'] = $order['totalPrice'];
-	$position ['vat'] = isset ($productMS[0]['vat']) ? $productMS[0]['vat'] : $productMS[0]['effectiveVat'];
+	$currentcyRate = $newOrder['convertedPrice'] / $newOrder['price'];
+	$position ['price'] = (int)((isset($newOrder['salePrice']) && $newOrder['salePrice'] != null ? $newOrder['salePrice'] : $newOrder['price']) * $currentcyRate);
 	$position ['assortment'] = array(
 		'meta' => $productMS[0]['meta']
 	);
 
 	$positions[] = $position;
 		
-	$date = DateTime::createFromFormat('Y-m-d\TH:i:s', explode ('.', $order['dateCreated'])[0]);
-	
+	$date = DateTime::createFromFormat('Y-m-d\TH:i:s\Z', $newOrder['createdAt']);
+	$attributes = array(
+		// тип оплаты
+		0 => array(
+			'meta' => array (
+				'href' => MS_ATTR . MS_PAYMENTTYPE_ATTR,
+				'type' => 'attributemetadata',
+				'mediaType' => 'application/json'
+			),
+			'value' => array(
+				'meta' => array(
+					'href' => MS_PAYMENTTYPE_SBERBANK_ONLINE,
+					'type' => 'customentity',
+					'mediaType' => 'application/json'
+				)
+			)
+		),
+		// время доставки
+		1 => array(
+			'meta' => array (
+				'href' => MS_ATTR . MS_DELIVERYTIME_ATTR,
+				'type' => 'attributemetadata',
+				'mediaType' => 'application/json'
+			),
+			'value' => array(
+				'meta' => array(
+					'href' => MS_DELIVERYTIME_9_21,
+					'type' => 'customentity',
+					'mediaType' => 'application/json'
+				)
+			)
+		),
+		// способ доставки
+		2 => array(
+			'meta' => array (
+				'href' => MS_ATTR . MS_DELIVERY_ATTR,
+				'type' => 'attributemetadata',
+				'mediaType' => 'application/json'
+			),
+			'value' => array(
+				'meta' => array(
+					'href' => MS_DELIVERY_VALUE_WB,
+					'type' => 'customentity',
+					'mediaType' => 'application/json'
+				)
+			)
+		)
+	);
+
 	$newOrdersMS[] = array(
-		'name' => 'WB' . (string)$order['orderId'],
+		'name' => 'WB' . (string)$newOrder['id'],
 		'organization' => array (
 			'meta' => array (
-				'href' => MS_KAORI,
+				'href' => MS_ULLO,
 				'type' => 'organization',
 				'mediaType' => 'application/json'
 			)
 		),
-		'externalCode' => (string)$order['orderId'],
+		'externalCode' => (string)$newOrder['id'],
 		'moment' => $date->format('Y-m-d H:i:s'),
 		'deliveryPlannedMoment' => $date->format('Y-m-d H:i:s'),
 		'applicable' => true,
-		'vatEnabled' => true,
-		'vatIncluded' => true,
+		'vatEnabled' => false,
+		'vatIncluded' => false,
 		'agent' => array(
 			'meta' => array (
 				'href' => MS_WB_AGENT,
@@ -108,7 +150,7 @@ foreach ($newOrders as $order)
 		),
 		'state' => array(
 			'meta' => array(
-			    'href' => MS_NEW_STATE,
+			    'href' => MS_MPNEW_STATE,
 				'type' => 'state',
 				'mediaType' => 'application/json'
 			)
@@ -129,105 +171,71 @@ foreach ($newOrders as $order)
 		),
 		'project' => array(
 			'meta' => array(
-				'href' => MS_PROJECT_WB,
+				'href' => MS_PROJECT_WB_ULLO,
 				'type' => 'project',
 				'mediaType' => 'application/json'
 			)
 		),
 		'positions' => $positions,
-		'attributes' => array(
-			// тип оплаты
-			0 => array(
-				'meta' => array (
-					'href' => MS_ATTR . MS_PAYMENTTYPE_ATTR,
-					'type' => 'attributemetadata',
-					'mediaType' => 'application/json'
-				),
-				'value' => array(
-					'meta' => array(
-						'href' => MS_PAYMENTTYPE_SBERBANK,
-						'type' => 'customentity',
-						'mediaType' => 'application/json'
-					)
-				)
-			),
-			// время доставки
-			1 => array(
-				'meta' => array (
-					'href' => MS_ATTR . MS_DELIVERYTIME_ATTR,
-					'type' => 'attributemetadata',
-					'mediaType' => 'application/json'
-				),
-				'value' => array(
-					'meta' => array(
-						'href' => MS_DELIVERYTIME_9_21,
-						'type' => 'customentity',
-						'mediaType' => 'application/json'
-					)
-				)
-			),
-			// способ доставки
-			2 => array(
-				'meta' => array (
-					'href' => MS_ATTR . MS_DELIVERY_ATTR,
-					'type' => 'attributemetadata',
-					'mediaType' => 'application/json'
-				),
-				'value' => array(
-					'meta' => array(
-					    'href' => MS_DELIVERY_VALUE0,
-						'type' => 'customentity',
-						'mediaType' => 'application/json'
-					)
-				)
-			),
-			// штрихкод
-			3 => array(
-				'meta' => array (
-					'href' => MS_ATTR . MS_BARCODE2_ATTR,
-					'type' => 'attributemetadata',
-					'mediaType' => 'application/json'
-				),
-				'value' => (string)$order['orderId']
-			),
-			// адрес доставки
-			4 => array(
-				'meta' => array (
-				    'href' => MS_ATTR . MS_ADDRESS_ATTR,
-					'type' => 'attributemetadata',
-					'mediaType' => 'application/json'
-				),
-			    'value' => (string)$order['deliveryAddress']
-			),
-			// ФИО
-			5 => array(
-				'meta' => array (
-				    'href' => MS_ATTR . MS_FIO_ATTR,
-					'type' => 'attributemetadata',
-					'mediaType' => 'application/json'
-				),
-			    'value' => (string)$order['userInfo']['fio']
-			),
-		    // телефон
-		    6 => array(
-		        'meta' => array (
-		            'href' => MS_ATTR . MS_PHONE_ATTR,
-		            'type' => 'attributemetadata',
-		            'mediaType' => 'application/json'
-		        ),
-		        'value' => (string)$order['userInfo']['phone']
-		    )
-		)
+		'attributes' => $attributes
 	);
-	
-	$changeStatus[] = array (
-		'orderId' => $order['orderId'],
-		'status' => 1
-	);
-	
 }
-if (count($newOrdersMS) > 0)
-	$ordersMSClass->createCustomerorder($newOrdersMS);
+if (count($newOrdersMS) > 0){
+	$result = $ordersMSClass->createCustomerorder($newOrdersMS);
+	if(!is_null($result)){
+		foreach ($newOrders as $newOrder){
+			if (array_search('WB' . $newOrder['id'], $ordersMSIDs) !== false)
+			{
+				continue;
+			}
+
+			$suppliesWBClass->addOrderToSupply($supplyOpen['id'], (string)$newOrder['id']);
+			#find order in result array and return item
+			$order = array_filter($result, function($item) use ($newOrder){
+				if (!isset($item['externalCode']))
+					return false;
+				return $item['externalCode'] == $newOrder['id'];
+			});
+			
+			$order = reset($order);
+			// get sticker
+			$stickers = $ordersWBClass->getStickers(array($newOrder['id']));
+			if (isset($stickers['stickers'][0]))
+			{
+				$order["attributes"][] = array(
+					'meta' => array (
+						'href' => MS_ATTR . MS_BARCODE_ATTR_ID,
+						'type' => 'attributemetadata',
+						'mediaType' => 'application/json'
+					),
+					'value' => (string)$stickers['stickers'][0]['barcode']
+				);
+				$order["attributes"][] = array(
+					'meta' => array (
+						'href' => MS_ATTR . MS_DELIVERYNUMBER_ATTR,
+						'type' => 'attributemetadata',
+						'mediaType' => 'application/json'
+					),
+					'value' => $stickers['stickers'][0]['partA'] . '-' . $stickers['stickers'][0]['partB']
+				);
+				$order["attributes"][] = array(
+					'meta' => array (
+						'href' => MS_ATTR . MS_WB_FILE_ATTR,
+						'type' => 'attributemetadata',
+						'mediaType' => 'application/json'
+					),
+					'file' => array(
+						'filename' => $stickers['stickers'][0]['orderId'] . '.png',
+						'content' => $stickers['stickers'][0]['file']
+					)
+				);
+			}
+			if (isset($order['id']))
+				$ordersMSClass->updateCustomerorder($order["id"], $order);
+		}
+	}
+}
+
 //if (count($changeStatus) > 0)
 //    $ordersWBClass->changeOrdersStatus($changeStatus);
 
