916713a7f3da3a32300ddab5e32a664f0cd72ee6
acidlord <acidlord@yandex.ru>
2025-03-30 10:04:47 -0400

    Added notifications to kosmos

---
+++ beru-kosmos/order/notification.php
@@ -0,0 +1,33 @@
+<?php
+	/**
+	 * Creates new order
+	 *
+	 * @class ControllerExtensionBeruOrder
+	 * @author GPOLYAN <acidlord@yandex.ru>
+	 */
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/settings.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/orders.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/log.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/api/apiMS.php');
+	$fake = isset($_GET['fake']) ? (bool)$_GET['fake'] : false;
+	
+	if (($_SERVER['REQUEST_METHOD'] != 'POST'))
+	{
+		header('HTTP/1.0 400 Bad Request');
+		echo 'Request must be POST';
+		return;
+	}
+	
+	$data = json_decode (file_get_contents('php://input'), true);
+	$logger = new Log('beru-kosmos - order - notification.log'); //just passed the file name as file_name.log
+	$logger->write(__LINE__ . ' data - ' . json_encode ($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+	
+	
+	$return = array(
+		'name' => 'beru-kosmos',
+		'version' => '1.0',
+		'time' => date('Y-m-dTH:i:sZ')
+	);
+	return $return;
+?>
