46808897554308f7e5d55a826afa3b57f2d1e89d
Georgy Polyan <georgy.polyan@myant.com>
2025-08-04 23:14:55 -0400

    fixed scrolling

--- js/compare.js
+++ js/compare.js
@@ -199,7 +199,7 @@ function buildCompareTable(data) {
     const compareBlock = document.getElementById('compare-block');
     const compareTable = document.getElementById('compare-table');
     const showDiffContainer = document.getElementById('show-diff-container');
-    compareBlock.style.display = 'table-cell';
+    compareBlock.style.display = '';
     compareTable.style.display = '';
     showDiffContainer.style.display = '';
     if (!Array.isArray(data) || data.length === 0) {
