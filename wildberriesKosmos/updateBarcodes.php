da99bb4debdc236b6eb8a310fe77adc1c18a7885
acidlord <acidlord@yandex.ru>
2025-09-08 00:11:44 +0300

    Troubleshooting

--- wildberriesKosmos/updateBarcodes.php
+++ wildberriesKosmos/updateBarcodes.php
@@ -12,7 +12,7 @@ $logger = new \Classes\Common\Log('wildberriesKosmos - updateBarcode.log');
 $ordersWBClass = new \Classes\Wildberries\v1\Orders('Kosmos');
 $suppliesWBClass = new \Classes\Wildberries\v1\Supplies('Kosmos');
 
-$filter = 'organization=' . MS_KOSMOS . ';agent=' . MS_WB_AGENT . ';' . MS_ATTR . MS_BARCODE_ATTR_ID . '=;';
+$filter = 'organization=' . MS_KOSMOS . ';agent=' . MS_WB_AGENT . ';' . MS_ATTR . MS_BARCODE_ATTR_ID . '=;name~=WB;';
 
 $ordersMSClass = new OrdersMS();
 $ordersMS = $ordersMSClass->findOrders($filter);
