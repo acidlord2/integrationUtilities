14a5dc171f81b28ab7b9e4b33b655f7223a7cca8
acidlord <acidlord@yandex.ru>
2025-09-07 20:15:16 +0300

    Added updateBarcodes for wb ulllo

---
+++ wildberriesUllo/updateBarcodes.php
@@ -0,0 +1,86 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/MS/ordersMS.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/MS/productsMS.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Wildberries/Orders.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Wildberries/Supplies.php');
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/classes/Common/Log.php');
+
+$logger = new \Classes\Common\Log('wildberriesUllo - getNewOrders.log');
+
+$startDate = date('Y-m-d', strtotime('-2 days')) . 'T00:00:00.000+03:00';
+$endDate = NULL;
+$status = 0;
+
+$ordersWBClass = new \Classes\Wildberries\v1\Orders('Ullo');
+$suppliesWBClass = new \Classes\Wildberries\v1\Supplies('Ullo');
+
+if (!count($newOrders))
+{
+	echo 'Processed 0 orders';
+	return;
+}
+
+foreach ($ordersIDs as $ordersID){
+	$filter .= 'organization=' . MS_ULLO . ';agent=' . MS_WB_AGENT . ';' . MS_ATTR . MS_BARCODE_ATTR_ID . '=;';
+}
+
+$ordersMSClass = new OrdersMS();
+$ordersMS = $ordersMSClass->findOrders($filter);
+
+// check if supply exists
+$supplyOpen = null;
+$supplies = $suppliesWBClass->getSupplies();
+foreach ($supplies as $supply)
+	if ($supply['closedAt'] == null)
+	{
+		$supplyOpen = $supply;
+		break;
+	}
+
+if ($supplyOpen === null)
+	$supplyOpen = $suppliesWBClass->createSupply('WB' . date('Y-m-d H:i:s'));
+
+$ordersMSIDs = array_column ($ordersMS, 'externalCode');
+$stickers = $ordersWBClass->getStickers(ordersMSIDs);
+$stickerIds = array_column ($stickers['stickers'], 'orderId');
+
+$msOrdersPostData = array();
+foreach ($ordersMS as &$orderMS)
+{
+	if(in_array($orderMS, $stickerIds)) {
+		$orderMS["attributes"][] = array(
+			'meta' => array (
+				'href' => MS_ATTR . MS_BARCODE_ATTR_ID,
+				'type' => 'attributemetadata',
+				'mediaType' => 'application/json'
+			),
+			'value' => (string)$stickers['stickers'][0]['barcode']
+		);
+		$orderMS["attributes"][] = array(
+			'meta' => array (
+				'href' => MS_ATTR . MS_DELIVERYNUMBER_ATTR,
+				'type' => 'attributemetadata',
+				'mediaType' => 'application/json'
+			),
+			'value' => $stickers['stickers'][0]['partA'] . '-' . $stickers['stickers'][0]['partB']
+		);
+		$orderMS["attributes"][] = array(
+			'meta' => array (
+				'href' => MS_ATTR . MS_WB_FILE_ATTR,
+				'type' => 'attributemetadata',
+				'mediaType' => 'application/json'
+			),
+			'file' => array(
+				'filename' => $stickers['stickers'][0]['orderId'] . '.png',
+				'content' => $stickers['stickers'][0]['file']
+			)
+		);
+		$msOrdersPostData[] = $orderMS;
+	}
+	
+	if (count ($msOrdersPostData))
+		$ordersMSClass->createCustomerorder($msOrdersPostData);
+	echo 'Updated ' . count ($msOrdersPostData) . ', of ' . count ($ordersMS);
+}
