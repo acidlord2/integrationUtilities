42e37ad4c2c2c419cd3f8184a5ca288cba7992a4
acidlord <acidlord@yandex.ru>
2025-09-07 21:50:10 +0300

    Troubleshooting

--- wildberriesUllo/updateBarcodes.php
+++ wildberriesUllo/updateBarcodes.php
@@ -12,7 +12,7 @@ $logger = new \Classes\Common\Log('wildberriesUllo - updateBarcode.log');
 $ordersWBClass = new \Classes\Wildberries\v1\Orders('Ullo');
 $suppliesWBClass = new \Classes\Wildberries\v1\Supplies('Ullo');
 
-$filter = 'organization=' . MS_ULLO . ';agent=' . MS_WB_AGENT . ';' . MS_ATTR . MS_BARCODE_ATTR_ID . '=;';
+$filter = 'organization=' . MS_ULLO . ';agent=' . MS_WB_AGENT . ';' . MS_ATTR . MS_BARCODE_ATTR_ID . '=;&limit=100';
 
 $ordersMSClass = new OrdersMS();
 $ordersMS = $ordersMSClass->findOrders($filter);
